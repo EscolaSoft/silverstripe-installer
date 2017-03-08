9d980cdccb1274a866d9aa2431c7b6d4def82704
Mateusz Wojczal <mateusz@qunabu.com>
2017-03-08 12:57:46 +0100

    composer update

--- install.php
+++ install.php
@@ -4,13 +4,10 @@
  ************************************************************************************
  **                                                                                **
  **  If you can read this text in your browser then you don't have PHP installed.  **
- **  Please install PHP 5.5.0 or higher.                                           **
+ **  Please install PHP 5.3.3 or higher, preferably PHP 5.3.4+.                    **
  **                                                                                **
  ************************************************************************************
  ************************************************************************************/
 
-if (!file_exists('framework') || !file_exists('framework/_config.php')) {
-	include "install-frameworkmissing.html";
-} else {
-	include('./framework/src/Dev/Install/install.php');
-}
+if (!file_exists('framework') || !file_exists('framework/_config.php')) include "install-frameworkmissing.html";
+else include('./framework/dev/install/install.php');
