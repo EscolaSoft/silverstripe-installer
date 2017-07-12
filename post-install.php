f38a66555118657789397295a72af999d19400f4
Sebastian Masztarowski <sebastian@qunabu.com>
2017-07-12 14:39:26 +0200

    post-install copy qunabu theme

--- post-install.php
+++ post-install.php
@@ -6,6 +6,31 @@
  * Time: 13:44
  */
 
+function rcopy($src, $dest){
+
+  // If source is not a directory stop processing
+  if(!is_dir($src)) return false;
+
+  // If the destination directory does not exist create it
+  if(!is_dir($dest)) {
+    if(!mkdir($dest)) {
+      // If the destination directory could not be created stop processing
+      return false;
+    }
+  }
+
+  // Open the source directory to read in files
+  $i = new DirectoryIterator($src);
+  foreach($i as $f) {
+    if($f->isFile()) {
+      copy($f->getRealPath(), "$dest/" . $f->getFilename());
+    } else if(!$f->isDot() && $f->isDir()) {
+      rcopy($f->getRealPath(), "$dest/$f");
+    }
+  }
+  return false;
+}
+
 function Delete($path)
 {
   if (is_dir($path) === true)
@@ -28,8 +53,15 @@ function Delete($path)
   return false;
 }
 
+function getCurrentDirectory() {
+  $path = dirname(__FILE__);
+  $position = strrpos($path,'/') + 1;
+  return substr($path,$position);
+}
+
 $dir = dirname(__FILE__).'/themes/qunabu/.git';
 echo $dir.' '.is_dir($dir);
+echo "\n";
 Delete($dir);
 
-//Delete();
+rcopy(dirname(__FILE__).'/themes/qunabu/', dirname(__FILE__).'/themes/'.strtolower(getCurrentDirectory()));
